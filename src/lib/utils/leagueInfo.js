2bd96fbecbb3db99784a7c00c5b9edd49b762c5e
swazy32 <sean.mcshane@comcast.net>
2024-07-27 13:08:41 -0500

    Update leagueInfo.js
    fixed trevor's photo

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -53,7 +53,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Charlotte, NC", // (optional)
       "bio": "Project Manager. Fantasy Footballer. Golfer. Craft Beer Connoisseur",
-      "photo": "/managers/trevor.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/trevor.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "det", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
