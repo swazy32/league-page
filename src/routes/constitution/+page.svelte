c96ac39ffdca70b9535c77a9185d4767413c5593
swazy32 <sean.mcshane@comcast.net>
2024-07-28 11:16:56 -0500

    Update +page.svelte
    update constitution (WIP)

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -114,8 +114,8 @@
     <h3 class="noUnderscore clickable" on:click={() => goToSection(two)}>Section 2: Drafting</h3>
     
         <h4 class="noUnderscore clickable" on:click={() => goToSection(twoOne)}>2.1 Draft Order</h4>
-        <h4 class="noUnderscore clickable" on:click={() => goToSection(twoTwo)}>2.2 Rookie Drafts</h4>
-        <h4 class="noUnderscore clickable" on:click={() => goToSection(twoThree)}>2.3 Draft Day Trades</h4>
+<!--        <h4 class="noUnderscore clickable" on:click={() => goToSection(twoTwo)}>2.2 Rookie Drafts</h4>   -->
+        <h4 class="noUnderscore clickable" on:click={() => goToSection(twoTwo)}>2.2 Draft Day Trades</h4>
     
     <h3 class="noUnderscore clickable" on:click={() => goToSection(three)}>Section 3: Scoring System</h3>
     
@@ -160,17 +160,20 @@
         <li>WR</li>
         <li>TE</li>
         <li>FLEX (RB/WR/TE)</li>
-        <li>FLEX (RB/WR)</li>
+        <li>FLEX (RB/WR/TE)</li>
         <li>D/ST</li>
         <li>K</li>
     </ul>
     
-    <p>18 Bench (Expanded by 5 between the keague draft and start of regular season)</p>
+    <p>5 Bench</p>
 
-    <p>1 IR Spots (Player must be labeled as IR, Out, or Covid in the Sleeper App)</p>
+    <p>1 IR Spot (Player must be labeled as IR, Out, or Covid in the Sleeper App)</p>
     
         <h4 bind:this={oneOneOne}>1.1.1 Position Maximums</h4>
-        
+
+        <p>There will be no positional limits for rosters as a whole. If a manager wants to hold 6 QBs and limit their own RB/WR depth, they are free to do so (at their own detriment).</p>
+
+<!--
         <div class="subBlock">
             <table class="positionMaximums">
                 <tr><td>QB</td><td class="right">3 active, 5 total</td></tr>
@@ -181,11 +184,11 @@
                 <tr><td>K</td><td class="right">3</td></tr>
             </table>
         </div>
-
+-->
 
     <h3 class="subSectionHeading" bind:this={oneTwo}>1.2 Trading</h3>
     
-    <p>Trading of players and draft picks (up to 3 drafts away) are allowed. Trades will process immediately and will be reversed if vetoed or under investigation for collusion. Tradable assets include players, draft picks, and FAAB $.</p>
+    <p>Trading of players and draft picks are allowed. Draft picks can only be included in trades if participating parties are paid in for the year(s) in which draft picks are included. This will prevent anyone from nuking their team (and the league) with no intentions of returning. Trades will process after 24 hours and will be canceled if vetoed or under investigation for collusion. Tradable assets include players, draft picks, and FAAB $. If you would like a trade to be expedited, message in the league chat and the commissioner may push the trade through after a league vote.</p>
     
     <h4 bind:this={oneTwoOne}>1.2.1 Trade Collusion</h4>
     <div class="subBlock">
@@ -206,43 +209,61 @@
 
     <h4 bind:this={oneTwoTwo}>1.2.2 Lending Players Prohibition</h4>
     <div class="subBlock">
-        <p>Any player traded from a team may not be re-acquired via trade within 6 weeks of the original trade. Any teams involved in this behavior will be subject to the aforementioned collusion punishments.</p>
+        <p>Any player traded from a team may not be re-acquired via trade within 3 weeks of the original trade. Any teams involved in this behavior will be subject to the aforementioned collusion punishments.</p>
     </div>
     
     <h4 bind:this={oneTwoThree}>1.2.3 Trade Deadline</h4>
     <div class="subBlock">
-        <p>The trade deadline will be set for the Saturday of the week 11 games of the NFL season.</p>
+        <p>The trade deadline will be set for the week 11 games of the NFL season.</p>
     </div>
     
     <h4 bind:this={oneTwoFour}>1.2.4 Trade Restrictions</h4>
     <div class="subBlock">
-        <p>You will not be able to trade draft picks that are more than 3 drafts away. For example, during the 2021 season you cannot trade any picks in or after the 2025 draft. You may only trade FAAB $ from the current season.</p>
+        <p>As previously stated, trades involving draft picks can only be made for years in which league dues are currently paid. You may only trade FAAB $ from the current season.</p>
     </div>
     
     <h4 bind:this={oneTwoFive}>1.2.5 Veto/Trade Process</h4>
     <div class="subBlock">
-        <p>A total of 7 veto votes are required to veto a trade. Vetoes are only appropriate in instances of collusion or an egregious mismatch of value (i.e. CMC for a backup kicker). Not liking a trade does not warrant a veto.</p>
+        <p>A total of 6 veto votes are required to veto a trade. Vetoes are only appropriate in instances of collusion or an egregious mismatch of value (i.e. CMC for a backup kicker). Not liking a trade does not warrant a veto.</p>
     </div>
     
     
     
     <h3 bind:this={oneThree}>1.3 Waiver Wire</h3>
     
-    <p>FAAB Waiver wire system. Each team will begin the season with $100. $0 bids are allowed. Bids will process Thursdays and Sundays at 9AM EST.</p>
+    <p>FAAB Waiver wire system. Each team will begin the season with $100. $0 bids are allowed. Bids will process Wednesdays 2AM CST.</p>
 
-    <p>FAAB budgets will reset each season in January.</p>
+    <p>FAAB budgets will reset each season.</p>
     
     
     <h2 class="sectionHeading" bind:this={two}>Section 2 Drafting</h2>
     
     <h3 bind:this={twoOne}>2.1 Draft Order</h3>
-    <p>The draft order each year will be a linear draft in reverse order of the previous year’s regular season standings.</p>
+    <p>The draft order each year will be a lottery system (NBA Draft style) based on reverse order of the previous year’s regular season standings (with the exception of the Champion, who will have the lowest odds regardless of regular season finish). The winner of the previous season's consolation championship will be awarded first selection of draft order position.</p>
 
+    <div class="subBlock">
+            <table class="positionMaximums">
+                <tr><td><u>Draft Order Selection</u></td><td class="right"><u>Previous Year Finish</u></td><td class="right"><u>Lottery Entries</u></td></tr>
+                <tr><td>1st Selection</td><td class="right">Consolation Champion</td><td class="right">Automatic First Selection</td></tr>
+                <tr><td>2nd Selection</td><td class="right">Lowest Remaining Regular Season Rank (10th or 9th)</td><td class="right">300 Entries</td></tr>
+                <tr><td>3rd Selection</td><td class="right">Lowest Remaining Regular Season Rank (9th or 8th)</td><td class="right">250 Entries</td></tr>
+                <tr><td>4th Selection</td><td class="right">Lowest Remaining Regular Season Rank (8th or 7th)</td><td class="right">200 Entries</td></tr>
+                <tr><td>5th Selection</td><td class="right">Lowest Remaining Regular Season Rank (6th or 5th)</td><td class="right">150 Entries</td></tr>
+                <tr><td>6th Selection</td><td class="right">Lowest Remaining Regular Season Rank (5th or 4th)</td><td class="right">125 Entries</td></tr>
+                <tr><td>7th Selection</td><td class="right">Lowest Remaining Regular Season Rank (4th or 3rd)</td><td class="right">100 Entries</td></tr>
+                <tr><td>8th Selection</td><td class="right">Lowest Remaining Regular Season Rank (3rd or 2nd)</td><td class="right">75 Entries</td></tr>
+                <tr><td>9th Selection</td><td class="right">Lowest Remaining Regular Season Rank (2nd or 1st)</td><td class="right">50 Entries</td></tr>
+                <tr><td>10th Selection</td><td class="right">Champion</td><td class="right">10 Entries</td></tr>
+            </table>
+        </div>
+
+<!--
     <h3 bind:this={twoTwo}>2.2 Rookie Drafts</h3>
     <p>Rokkie drafts will consist of 3 round of drafting, with 5 minutes alotted for each pick. To make room for the incoming players, benches will be expanded by (5) spots. These spots will be removed in mid-August, at which point you will need to have cut the necessary players.</p>
     <p>Any spots not filled during the draft, can be filled immediately afterwards from the waiver wire.</p>
-    
-    <h3 bind:this={twoThree}>2.3 Draft Day Trades</h3>
+-->
+
+    <h3 bind:this={twoTwo}>2.2 Draft Day Trades</h3>
     <p>If a trade is being made during the draft, one or both parties can alert the commissioner to pause the draft to allow for extra time to negotiate the trade.</p>
     
     <h2 class="sectionHeading" bind:this={three}>Section 3 Scoring System</h2>
