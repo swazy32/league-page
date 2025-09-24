66d652c99a981741d885e6c4dbdc6c60c33e3a6b
Sean McShane <sean.mcshane@comcast.net>
2025-09-23 20:34:59 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -225,7 +225,7 @@ export const managers = [
       "bio": "Unemployed drive-thru manager at Long John Silver’s. Part time international arms dealer. EXTREMELY serious taker of things. Jameis Winston and I share favorite meals, W’s. ",
       "photo": "/managers/kevin.jpeg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": "RIGHT OUT THE WOMB!", // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "chi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "ari", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Kamala Harris", // Can be anything (usually your rival's name)
